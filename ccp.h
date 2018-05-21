312ee4a22d810aa66826ec08cad1f4220dbcd0d1
Frank Cangialosi <frankc@csail.mit.edu>
2018-05-21 15:03:25 -0400

    ccp.h: include serialize.h instead of redefining int types to fix compiler warnings on osx

--- ccp.h
+++ ccp.h
@@ -51,10 +51,7 @@
     #include <linux/module.h>
 #endif
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+#include "serialize.h"
 
 #ifdef __CPLUSPLUS__
 extern "C" {
